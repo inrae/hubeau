304acdd4443e882aee8e1878eb060df1f1b1ce48
David Dorchies <14124454+DDorch@users.noreply.github.com>
2023-08-19 22:13:01 +0200

    feat: add API qualite_rivieres
    Refs #9

---
+++ R/get_qualite_rivieres.R
@@ -0,0 +1,147 @@
+#' Retrieve data from API "Qualité physico-chimique des cours d'eau"
+#'
+#' @description
+#' The "Quality of rivers" API data comes from the Naïades database.
+#' Available for the whole of France (including the French overseas departments
+#' and territories), they relate to the results of measurements of the
+#' physico-chemical quality of rivers and water bodies transmitted by the Water
+#' Agencies.
+#'
+#' Available endpoints are:
+#'
+#' - `get_qualite_rivieres_station` retrieves physico-chemical measuring stations
+#' - `get_qualite_rivieres_operation` retrieves physico-chemical operations
+#' - `get_qualite_rivieres_analyse` retrieves physico-chemical analysis
+#' - `get_qualite_rivieres_condition_environnementale` retrieves physico-chemical
+#' environmental conditions for analyses (please note that pagination is based
+#' on the number of analyses)
+#'
+#' See the API documentation for available filter parameters: \url{https://hubeau.eaufrance.fr/page/api-qualite-cours-deau}
+#'
+#' @inheritParams doApiQuery
+#' @inherit convert_list_to_tibble return return
+#'
+#' @export
+#' @rdname get_qualite_rivieres
+#'
+#' @examples
+#' \dontrun{
+#' # Tutorial provided by https://hubeau.eaufrance.fr/page/api-qualite-cours-deau-tuto
+#'
+#' # List of stations in Longuyon
+#' get_qualite_rivieres_station(libelle_commune="Longuyon")
+#' # ... with field selection
+#' get_qualite_rivieres_station(
+#'   libelle_commune = "Longuyon",
+#'   fields = c("code_station",
+#'              "libelle_station",
+#'              "localisation_precise",
+#'              "code_commune",
+#'              "libelle_commune",
+#'              "code_cours_eau",
+#'              "nom_cours_eau")
+#' )
+#'
+#' # Query results of nitrate analyses at the station since 2013
+#' get_qualite_rivieres_analyse(
+#'   code_station = "02115725",
+#'   libelle_parametre = "Nitrates",
+#'   date_debut_prelevement = "2013-01-01",
+#'   code_qualification = 1,
+#'   fields = c("code_station",
+#'              "libelle_station",
+#'              "code_parametre",
+#'              "libelle_parametre",
+#'              "date_prelevement",
+#'              "resultat",
+#'              "symbole_unite",
+#'              "code_remarque",
+#'              "mnemo_remarque",
+#'              "code_statut",
+#'              "mnemo_statut",
+#'              "code_qualification",
+#'              "libelle_qualification")
+#' )
+#'
+#' # dates sorted from most recent to oldest
+#' get_qualite_rivieres_analyse(
+#'   code_station = "02115725",
+#'   libelle_parametre = "Nitrates",
+#'   date_debut_prelevement = "2013-01-01",
+#'   code_qualification = 1,
+#'   fields = c("code_station",
+#'              "libelle_station",
+#'              "code_parametre",
+#'              "libelle_parametre",
+#'              "date_prelevement",
+#'              "resultat",
+#'              "symbole_unite",
+#'              "code_remarque",
+#'              "mnemo_remarque",
+#'              "code_statut",
+#'              "mnemo_statut",
+#'              "code_qualification",
+#'              "libelle_qualification"),
+#'  sort = "desc"
+#' )
+#'
+#' # Nitrate analysis results since 1 August 2016 for 5 stations
+#' get_qualite_rivieres_analyse(
+#'   code_station = c("02115725",
+#'                    "02115650",
+#'                    "02115685",
+#'                    "02115700",
+#'                    "02115715"),
+#'   libelle_parametre = "Nitrates",
+#'   date_debut_prelevement = "2016-08-01",
+#'   code_qualification = 1,
+#'   fields = c("code_station",
+#'              "libelle_station",
+#'              "libelle_parametre",
+#'              "date_prelevement",
+#'              "resultat",
+#'              "symbole_unite",
+#'              "code_remarque"),
+#'  sort = "desc"
+#' )
+#'
+#' # Geographical search by distance
+#' get_qualite_rivieres_station(longitude = 5.62,
+#'                              latitude = 49.44,
+#'                              distance = 8,
+#'                              fields = c("code_station", "libelle_station"))
+#'
+#' }
+get_qualite_rivieres_analyse <- function(...) {
+  l <- doApiQuery(api = "qualite_rivieres",
+                  endpoint = "analyse_pc",
+                  ...)
+  convert_list_to_tibble(l)
+}
+
+#' @export
+#' @rdname get_qualite_rivieres
+get_qualite_rivieres_condition_environnementale <- function(...) {
+  l <- doApiQuery(api = "qualite_rivieres",
+                  endpoint = "condition_environnementale_pc",
+                  ...)
+  convert_list_to_tibble(l)
+}
+
+#' @export
+#' @rdname get_qualite_rivieres
+get_qualite_rivieres_operation <- function(...) {
+  l <- doApiQuery(api = "qualite_rivieres",
+                  endpoint = "operation_pc",
+                  ...)
+  convert_list_to_tibble(l)
+}
+
+#' @export
+#' @rdname get_qualite_rivieres
+get_qualite_rivieres_station <- function(...) {
+  l <- doApiQuery(api = "qualite_rivieres",
+                  endpoint = "station_pc",
+                  ...)
+  convert_list_to_tibble(l)
+}
