99900e711010badcc274267e3704189a5dd319e9
David Dorchies <14124454+DDorch@users.noreply.github.com>
2024-02-22 10:40:38 +0100

    fix: warning @docType "package" is deprecated
    Refs #39

--- R/hubeau.R
+++ R/hubeau.R
@@ -1,10 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
 #' @title hubeau: A package for retrieving data on the French databases on water 'Hub'Eau'
 #'
 #' @description
 #' ```{r child = "man-roxygen/hubeau.md"}
 #' ```
 #'
-#' @docType package
 #' @import utils
 #' @name hubeau
 #'
