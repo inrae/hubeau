006610dd4d08fdefd27641445b7275b82723eeca
David Dorchies <14124454+DDorch@users.noreply.github.com>
2024-12-01 14:07:42 +0100

    fix(get_hydrometrie_observations_tr): crashes when returns empty result
    Closes #47

--- R/get_hydrometrie.R
+++ R/get_hydrometrie.R
@@ -64,7 +64,7 @@ get_hydrometrie_observations_tr  <- function(...,
     endpoint = "observations_tr",
     ...
   )
-  if(!is.null(l)) {
+  if (length(l) > 0) {
     l <- lapply(l, function(x) {
       if (entities == "station") {
         if (is.null(x$code_station)) {
@@ -78,9 +78,8 @@ get_hydrometrie_observations_tr  <- function(...,
       return(x)
     })
     l[sapply(l, is.null)] <- NULL
-    l <- convert_list_to_tibble(l)
   }
-  return(l)
+  return(convert_list_to_tibble(l))
 }
 
 
